7eebfe812e27fdef9d5a349472fc09752337bc7d
mufidshaikh947-ai <mufidshaikh947@gmail.com>
2026-08-09 12:05:40 +0530

    mail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,3 +1,10 @@
+
+console.log({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    user: process.env.EMAIL_USER,
+    passExists: !!process.env.EMAIL_PASS
+});
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
