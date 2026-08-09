f655d179a1edad39b71d849944b038c751876fec
mufidshaikh947-ai <mufidshaikh947@gmail.com>
2026-08-09 12:45:06 +0530

    sending mail through different port

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -10,7 +10,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: false,
+    secure: true ,
 
     auth: {
         user: process.env.EMAIL_USER,
